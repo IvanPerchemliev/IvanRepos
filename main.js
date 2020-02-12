3b68277ef114e397169fcd6ad4fe1e7fd31f80e8
IvanPerchemliev <60983435+IvanPerchemliev@users.noreply.github.com>
2020-02-12 21:59:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+var array = new Array(2, 3, 42, 5, 6, 23, 4, 5, 6, 78, 8, 1, 2, 3, 55, 22, 1, 2, );
+var newArray = [];
+for (var i = 0; i < array.length; i++) {
+  if (array[i] > 21) {
+    newArray.push(array[i]);
+  }
+}
+console.log(newArray);
